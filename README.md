4b9bba9f56ef97e7be75d57ff86bc63de258347d
Steven Dong <sunlight75@gmail.com>
2023-04-11 04:00:12 -0700

    first push

---
+++ README.md
@@ -0,0 +1,9 @@
+# pixel art sprite splitter/merger #
+Slicing sprites made for RPG Maker into seprate files that can be used by other game engines (such as Unity)
+
+To use
+1. Put the original sprite files into input folder
+2. Modify the input settings in line 2 - 13
+3. Run the script and the result will be saved to merged folder
+
+The sample sprite in this repo is from https://zunko.jp/con_illust.html#illust_03500
---
+++ SpriteProcessor.py
@@ -0,0 +1,96 @@
+# %%
+from PIL import Image
+import os
+
+# %%
+pixW = 48  # height of each sprite
+pixH = 48  # width of each sprite
+modW = 0   # if the height and width are not the same, use this to adding padding to both side
+modH = 0
+totalImgToMerge = 3  # images to merge into one horizontally
+imgType = ".png"
+inputDir = "./input/"
+outputDir = "./output/"
+mergedDir = "./merged/"
+# For standard RPG Maker sprites use line 16 and 86, if not standard RPG Maker spirit use line 17 and 87
+stateName = ["front", "left", "right", "back", "death"]
+# spriteRowCount = 0
+
+# Create folders to hold output and merged images
+if not os.path.exists("./merged"):
+    os.mkdir("./merged")
+
+if not os.path.exists("./output"):
+    os.mkdir("./output")
+
+
+# Open the image
+imgFiles = [f for f in os.listdir(inputDir) if f.endswith(imgType)]
+
+# Initialize a counter for the output file names
+count = 0
+
+for image in imgFiles:
+    img = Image.open(inputDir + image)
+
+    # get image Name
+    imageName = image[:-4]
+
+    # Get the width and height of the image
+    width, height = img.size
+
+    # Calculate the number of rows and columns
+    rows = height // pixH
+    cols = width // pixW
+
+    # Slice the image into 48 by 48 pixels and save each slice
+    for row in range(rows):
+        for col in range(cols):
+            left = col * pixW
+            top = row * pixH
+            right = left + pixW
+            bottom = top + pixH
+            crop = img.crop((left, top, right, bottom))
+            filename = outputDir + \
+                f"{str(row).zfill(3)}_{str(col).zfill(3)}{imgType}"
+            crop.save(filename)
+
+    # Get a list of all the image files in the outputDir
+    imgFiles = [f for f in os.listdir(outputDir) if f.endswith(imgType)]
+
+    # Sort the image files by name
+    imgFiles.sort()
+
+    # Assuming all images are the same size
+    modImgW = pixW + modW*2
+    modImgH = pixH + modH*2
+
+    # create an empty image for merging
+    merged = Image.new("RGBA", ((modImgW*totalImgToMerge, modImgH)))
+
+    while len(imgFiles) >= totalImgToMerge:
+        for x in range(totalImgToMerge):
+            # Open the image to be merged
+            img = Image.open(outputDir + imgFiles.pop(0))
+
+            # Create a new image to deal with height/width mismatch
+            imgNew = Image.new('RGBA', (modImgW, modImgH), (0, 0, 0, 0))
+
+            # past the original to the center of the new image
+            imgNew.paste(img, (modW, modH))
+
+            # Paste the modified image to the final image
+            merged.paste(imgNew, (modImgW*x, 0))
+
+        # Save the merged image with the next output filename
+        mergedFilename = mergedDir + f"{imageName}_{stateName[count]}{imgType}"
+        # mergedFilename = mergedDir + f"{imageName}_{str(count).zfill(3)}_merged{imgType}"
+        merged.save(mergedFilename)
+        count += 1
+
+    # Remove all the image files from the outputDir for the next image
+    for f in os.listdir(outputDir):
+        os.remove(os.path.join(outputDir, f))
+
+    # increment leadingCount to distinguish images from different source files
+    count = 0
